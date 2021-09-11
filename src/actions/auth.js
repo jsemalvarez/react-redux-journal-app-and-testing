17f878f712f6245ebd769589562f649cb03286a6
jsemalvarez <jsemalvarez@gmail.com>
2021-09-11 20:15:07 -0300

    configurar firebase y google Sign-in. Fromulario y manejo de errores

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,9 +1,20 @@
+import { signInWithPopup, auth, googleAuthProvider } from "../firebase/firebase-config"
 import { types } from "../types/types"
 
 export const startLoginEmailPassword = ( email, password ) => {
 
     return( dispatch ) => {
         dispatch( login( 555, 'Jose' ) )
+    }
+}
+
+export const startGoogleLogin = () => {
+    return( dispatch ) => {
+
+        signInWithPopup(auth, googleAuthProvider)
+            .then( ({ user }) => {
+                dispatch( login( user.uid, user.displayName))
+            }) 
     }
 }
 
---
+++ src/firebase/firebase-config.js
@@ -0,0 +1,29 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore/lite';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyApuN6QMeFSAqdQ_amp-hPrCIpSFRoicI0",
+    authDomain: "jsm-proyecto-de-pruebas.firebaseapp.com",
+    databaseURL: "https://jsm-proyecto-de-pruebas.firebaseio.com",
+    projectId: "jsm-proyecto-de-pruebas",
+    storageBucket: "jsm-proyecto-de-pruebas.appspot.com",
+    messagingSenderId: "765649458073",
+    appId: "1:765649458073:web:0a29a84e3819029908b908"
+};
+  
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+
+const db = getFirestore(firebase);
+const googleAuthProvider = new GoogleAuthProvider();
+const auth = getAuth();
+
+export {
+    db,
+    googleAuthProvider,
+    auth,
+    signInWithPopup,
+    firebase
+}
